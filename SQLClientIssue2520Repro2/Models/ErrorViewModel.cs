203d1cbbc6ec46e8598f8d2773693afe922f0e52
brood <brood@HUGCOMP>
2024-07-23 12:38:58 +0200

    Add project files.

---
+++ SQLClientIssue2520Repro2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SQLClientIssue2520Repro2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
